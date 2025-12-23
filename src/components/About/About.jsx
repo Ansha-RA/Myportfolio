5b4626caa0c8e764b59654fea01f69f5f3abc228
Ansha shabbir <anshamansoori@gmail.com>
2025-12-23 21:56:48 +0530

    portfolio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import Tilt from "react-parallax-tilt";
-import profileImg from "../../assets/profile.jpeg";
+import profileImg from "../../assets/AnshaProfile.jpeg";
 
 
 const About = () => {
